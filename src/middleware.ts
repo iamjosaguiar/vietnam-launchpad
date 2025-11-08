1341451c362cf00b773186d36f64195528bf853c
iamjosaguiar <jos@iamjosaguiar.com>
2025-11-09 00:42:06 +0700

    Re-add localeDetection: false to prevent auto redirects

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,12 @@
 import createMiddleware from 'next-intl/middleware';
 import { routing } from './i18n/config';
 
-export default createMiddleware(routing);
+export default createMiddleware({
+  ...routing,
+  // Disable automatic locale detection from browser Accept-Language header
+  // Language switching is handled manually via LanguageSwitcher component
+  localeDetection: false
+});
 
 export const config = {
   // Match all pathnames except for
